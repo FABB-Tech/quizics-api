7ea018a2eb986dcc769ddf0e6c9e933ba3d3eff2
Benito Luongo <luongov13@gmail.com>
2018-02-11 23:29:56 -0600

    initializing connection to Firebase

---
+++ src/database/firebase.ts
@@ -0,0 +1,11 @@
+import * as admin from 'firebase-admin'
+
+import serviceAccount from './serviceAccountKey'
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://quick-quizics.firebaseio.com"
+});
+
+export const db = admin.firestore()
+export const auth = admin.auth()
